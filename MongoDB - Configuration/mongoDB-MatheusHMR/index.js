8436c89114a6fa69395ad8470cf3b5086dfd1e50
MatheusHMR <matheus.henrimr@gmail.com>
2023-11-15 16:35:01 -0300

    feat README.md with old code for MongoDB

--- MongoDB - Configuration/mongoDB-MatheusHMR/index.js
+++ MongoDB - Configuration/mongoDB-MatheusHMR/index.js
@@ -1,41 +1,36 @@
-// const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-// const dbUrl = 'mongodb+srv://MatheusHMR:UgTZRaETuTHGCo2Y@clusterteste.x27fupa.mongodb.net/?retryWrites=true&w=majority';
+const app = express();
+app.use(bodyParser.json());
 
-// const connectionParams = {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }
-
-// mongoose.connect(dbUrl, connectionParams).then(() => {
-//     console.info("Connected to the DB");
-// }).catch((e) => {
-//     console.log("Error:", e);
-// });
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://MatheusHMR:UgTZRaETuTHGCo2Y@clusterteste.x27fupa.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+const Schema = mongoose.Schema;
+const dataSchema = new Schema({
+  name: String,
+  value: Number
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+const Data = mongoose.model('Data', dataSchema);
+
+app.post('/data', (req, res) => {
+    const newData = new Data({
+      name: req.body.name,
+      value: req.body.value
+    });
+
+    newData.save()
+    .then(item => res.json(item))
+    .catch(err => {
+      console.log(err);
+      res.status(500).send("Server error");
+    });
+  });
+
+  const port = 4000; // Replace with your port number
+  app.listen(port, () => console.log(`Server started on port ${port}`));
